de7e315595fbf31c2ecfe2cbf3ed9fb5c3abdc0d
magnusolafur <magnusm12@ru.is>
2015-05-09 18:01:58 +0100

    Added file storage in database

---
+++ src/Hooli/Models/Filetype.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hooli.Models
+{
+    public enum FileType
+    {
+        ProfilePicture = 1, Photo
+    }
+}
