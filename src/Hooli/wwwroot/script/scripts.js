75db0db46969882d2b586870577f0fd5eb1cf577
saraa12 <saraa12@ru.is>
2015-05-11 14:21:11 +0000

    Working on the Filtering buttons on the feed, added files for the group view, creating a group, viewing a single group and more

--- src/Hooli/wwwroot/script/scripts.js
+++ src/Hooli/wwwroot/script/scripts.js
@@ -1,5 +1,23 @@
 ﻿
 
 $(document).ready(function () {
-    $(".posts-type button:nth-child(1)").removeClass("btn-selected").addClass("btn-option");
+    //TODO: add a check so that if a person clicks a previously selected button classes aren't added
+    $(".posts-type button:nth-child(1)").click(function () {
+        $(".posts-type button:nth-child(1)").removeClass("btn-option").addClass("btn-selected");
+        $(".posts-type button:nth-child(2)").removeClass("btn-selected").addClass("btn-option");
+    });
+    $(".posts-type button:nth-child(2)").click(function () {
+        $(".posts-type button:nth-child(1)").removeClass("btn-selected").addClass("btn-option");
+        $(".posts-type button:nth-child(2)").removeClass("btn-option").addClass("btn-selected");
+    });
+    $(".posts-orderby button:nth-child(1)").click(function() {
+        $(".posts-orderby button:nth-child(1)").removeClass("btn-option").addClass("btn-selected");
+        $(".posts-orderby button:nth-child(2)").removeClass("btn-selected").addClass("btn-option");
+    });
+    $(".posts-orderby button:nth-child(2)").click(function () {
+        $(".posts-orderby button:nth-child(1)").removeClass("btn-selected").addClass("btn-option");
+        $(".posts-orderby button:nth-child(2)").removeClass("btn-option").addClass("btn-selected");
+    });
+
+    
 });
