8d6fe7b5c884ca648813ea0edf5fdc79545c9d49
saraa12 <saraa12@ru.is>
2015-05-11 21:23:30 +0000

    Something to do with the sorting

--- src/Hooli/Controllers/HomeController.cs
+++ src/Hooli/Controllers/HomeController.cs
@@ -26,8 +26,8 @@ namespace Hooli.Controllers
         public IMemoryCache Cache { get; set; }
         //
         // GET: /Home/
-        public async Task<IActionResult> Index()
-        {
+        public async Task<IActionResult> Index(string sortOrder)
+        { 
             // Get most popular Posts, from following users
             //var Posts = await Cache.GetOrSet("top", async context =>
             //{
