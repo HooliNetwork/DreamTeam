13f6c49aa2f0ecb4e46e88041943aaba811eef73
magnusolafur <magnusm12@ru.is>
2015-05-12 22:20:45 +0100

    User can now login using username

--- src/Hooli/Models/AccountViewModels.cs
+++ src/Hooli/Models/AccountViewModels.cs
@@ -66,8 +66,7 @@ namespace Hooli.Models
     public class LoginViewModel
     {
         [Required]
-        [EmailAddress]
-        public string Email { get; set; }
+        public string Username { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
