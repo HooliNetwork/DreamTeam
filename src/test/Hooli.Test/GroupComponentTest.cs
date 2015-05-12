6cf96c2cd49e78607d35d6318d75cf5397413573
Benedikt Sigurleifsson <benedikt12@ru.is>
2015-05-12 16:00:36 -0700

    Setting things up for unit testing the components

---
+++ src/test/Hooli.Test/GroupComponentTest.cs
@@ -0,0 +1,25 @@
+﻿using Hooli.Models;
+using Microsoft.Framework.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hooli.Test
+{
+    public class GroupComponentTest
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public GroupComponentTest()
+        {
+            var services = new ServiceCollection();
+
+            services.AddEntityFramework()
+                      .AddInMemoryStore()
+                      .AddDbContext<HooliContext>();
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+    }
+}
