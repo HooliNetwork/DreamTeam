03896e967b8a8c7df1a0e2d2882b0eee0d29faad
magnusolafur <magnusm12@ru.is>
2015-05-13 23:34:43 +0100

    Added Profile page for user

--- src/Hooli/Controllers/ProfileController.cs
+++ src/Hooli/Controllers/ProfileController.cs
@@ -33,37 +33,66 @@ namespace Hooli.Controllers
             UserManager = userManager;
         }
 
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<JsonResult> EditProfile([FromForm]EditProfileData data, CancellationToken requestAborted)
+        //{
+
+        //    var profileData = await GetCurrentUserAsync();
+
+        //    if ((data.FirstName != null) && (data.FirstName.ToString().Length > 0))
+        //    {
+        //        profileData.FirstName = data.FirstName;
+        //    }
+
+        //    if ((data.LastName != null) && (data.LastName.ToString().Length > 0))
+        //    {
+        //        profileData.LastName = data.LastName;
+        //    }
+
+        //    if ((data.DateOfBirth != null) && (data.DateOfBirth.ToString().Length > 0))
+        //    {
+        //        profileData.DateOfBirth = data.DateOfBirth;
+        //    }
+
+        //    await DbContext.SaveChangesAsync(requestAborted);
+        //    return Json("error-not-fully-implemented");
+        //}
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<JsonResult> EditProfile([FromForm]EditProfileData data, CancellationToken requestAborted)
+        public async Task<IActionResult> EditProfile(IFormCollection form, CancellationToken requestAborted)
         {
 
             var profileData = await GetCurrentUserAsync();
 
-            if ((data.FirstName != null) && (data.FirstName.ToString().Length > 0))
-            {
-                profileData.FirstName = data.FirstName;
-            }
+            profileData.FirstName = Convert.ToString(form["first_name"]);
+            profileData.LastName = Convert.ToString(form["last_name"]);
+            profileData.DateOfBirth = Convert.ToDateTime(form["date_birth"]);
 
-            if ((data.LastName != null) && (data.LastName.ToString().Length > 0))
-            {
-                profileData.LastName = data.LastName;
-            }
 
-            if ((data.DateOfBirth != null) && (data.DateOfBirth.ToString().Length > 0))
-            {
-                profileData.DateOfBirth = data.DateOfBirth;
-            }
 
             await DbContext.SaveChangesAsync(requestAborted);
             return Json("error-not-fully-implemented");
         }
 
         // GET: /<controller>/
-        public async Task<IActionResult> Index()
+        [HttpGet]
+        public async Task<IActionResult> Index(string id)
         {
-            return View();
+            var q = Request.QueryString;
+            Console.WriteLine(q);    
+            Console.WriteLine("Inside index " + id);
+
+            var user = await DbContext.Users.SingleAsync(u => u.UserName == id);
+            if(user == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(user);
         }
+
 
         private async Task<ApplicationUser> GetCurrentUserAsync()
         {
