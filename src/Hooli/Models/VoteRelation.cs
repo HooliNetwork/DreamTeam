556780b7f4dd687e813f274f9a6c4f227c1a6a9a
Joiblumen <joiblumen@gmail.com>
2015-05-13 12:37:27 +0000

    Implement voting functionality

---
+++ src/Hooli/Models/VoteRelation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hooli.Models
+{
+    public class VoteRelation
+    {
+        public string PostId { get; set; }
+        public string UserId { get; set; }
+        public virtual Post Post { get; set; }
+        public virtual ApplicationUser Voter { get; set; }
+    }
+}
