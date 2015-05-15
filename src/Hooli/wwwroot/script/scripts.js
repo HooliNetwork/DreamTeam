6822a9d2bf1f6139bdc50ef3629746bff425c9b9
hoddmachine <hordur.ragnarsson@gmail.com>
2015-05-15 05:28:04 +0000

    Remove duplicate in scritpts causing search filters to malfunction

--- src/Hooli/wwwroot/script/scripts.js
+++ src/Hooli/wwwroot/script/scripts.js
@@ -47,19 +47,6 @@ $(document).ready(function () {
     $(".create-post-button").click(function(){
         $(".new-post-container").toggleClass('open');
     });
-    
-    $(".search-filters button").click(function() {
-        $(this).siblings().not(".btn-option").toggleClass("btn-option");
-        $(this).toggleClass("btn-option");
-        var filter = $(this).attr("name");
-
-        if(filter === "s_all") {
-            $(".search-results").show(100);
-        } else {
-            $(".search-results").not('.' + filter).hide(100);            
-            $('.' + filter).show(100);
-        }
-    })
 
     $(function () {
         $('[data-toggle="tooltip"]').tooltip();
@@ -79,7 +66,6 @@ $(document).ready(function () {
         $(".reply-to-post").toggleClass('open');
     });
 
-    // Search results filter
     $(".search-filters button").click(function () {
         $(this).siblings().not(".btn-option").toggleClass("btn-option");
         $(this).toggleClass("btn-option");
